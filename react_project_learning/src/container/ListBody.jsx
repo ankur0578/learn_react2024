11796005666bd01195a16dac149564342310db88
Ankur Chauhan <81966472+ankur0578@users.noreply.github.com>
2024-01-25 06:35:15 +0000

    adding more code

---
+++ react_project_learning/src/container/ListBody.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+const ListBody = () => {
+    const [todoItem , setTodoItems] = useState([])
+    const [item, setItem] = useState("")
+
+    const handleOnChange = (e) => {
+      setItem(e.target.value)
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if(item.trim() != ""){
+            setTodoItems([...todoItem, item]);
+        }
+        setItem("")
+        console.log(e)
+    }
+   
+    return (
+        <div className='listBody'>
+           <form onSubmit={handleSubmit}>
+            <input type="text" name="item" id="item" value={item} onChange={handleOnChange}/>
+            <button type='submit'>+</button>
+           </form>
+            {
+            todoItem.map((item, index) =>(
+                <div key={index}>{item}</div>
+            ))
+          }
+        </div>
+    );
+}
+
+export default ListBody;
